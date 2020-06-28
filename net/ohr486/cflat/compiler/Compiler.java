ef1ac0c4542b958649559776ab1def8c6c5055ec
ohr486 <ohr486@gmail.com>
2020-06-28 20:31:31 +0900

    add base compiler

---
+++ net/ohr486/cflat/compiler/Compiler.java
@@ -0,0 +1,15 @@
+package net.ohr486.cflat.compiler;
+
+import java.util.*;
+import java.io.*;
+
+public class Compiler {
+    static public void main(String[] args) {
+        new Compiler("cbc").commandMain(args);
+    }
+
+    public Compiler(String programName) {}
+
+    public void commandMain(String[] args) {
+    }
+}
